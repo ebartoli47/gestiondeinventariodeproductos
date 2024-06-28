eb5cbac6c3b9608312f176d9c0656c5e99dbc919
Laureano <powerexe_@hotmail.com>
2024-06-28 11:18:22 -0300

    html css y javascript basicos

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,41 @@
-
+//Datos Iniciales
+let inventario = [
+    {
+        nombre: "Laptop",
+        categoria: "Electrónica",
+        precio: 1000,
+        cantidad: 10,
+        codigo: "P001"
+    },
+    {
+        nombre: "Smartphone",
+        categoria: "Electrónica",
+        precio: 700,
+        cantidad: 25,
+        codigo: "P002"
+    },
+    {
+        nombre: "Teclado",
+        categoria: "Accesorios",
+        precio: 50,
+        cantidad: 50,
+        codigo: "P003"
+    },
+    {
+        nombre: "Monitor",
+        categoria: "Electrónica",
+        precio: 300,
+        cantidad: 15,
+        codigo: "P004"
+    },
+    {
+        nombre: "Silla de Oficina",
+        categoria: "Muebles",
+        precio: 150,
+        cantidad: 20,
+        codigo: "P005"
+    }
+];
 
 
 
