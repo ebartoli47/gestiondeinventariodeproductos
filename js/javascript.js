3d32ce81d54982656904fa3db8d0f33a5d2dee69
Laureano <powerexe_@hotmail.com>
2024-06-28 15:00:56 -0300

    mostrar agregar y eliminar productos

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-//Datos Iniciales
+// Datos Iniciales
 let inventario = [
     {
         nombre: "Laptop",
@@ -37,5 +37,127 @@ let inventario = [
     }
 ];
 
+//----------------------------------------//
 
+let tabla = document.querySelector('#elementosTabla');
+let buttonAgregar = document.querySelector('#buttonAgregar')
 
+//MOSTRAR PRODUCTOS
+function mostrarProductos() {
+
+    tabla.innerHTML = '';
+
+    for (let producto of inventario) {
+
+        //Creacion de boton eliminar
+        let botonEliminar = document.createElement("button")
+        botonEliminar.textContent="Eliminar"
+        //Ejecucion de la funcion Eliminar Productos
+        botonEliminar.addEventListener('click', function() {
+            eliminarProducto(producto.codigo);
+        });
+
+        //Creacion de boton actualizar cantidad
+        let botonActualizarCantidad = document.createElement("button")
+        botonActualizarCantidad .textContent="Actualizar Cantidad"
+        //Ejecucion de la funcion Eliminar Productos
+        botonActualizarCantidad .addEventListener('click', function() {
+            actualizarCantidadProducto(producto.codigo, nuevaCantidad);
+        });
+        
+        // Crear fila para cada producto
+        let filaProducto = document.createElement('tr');
+        filaProducto.innerHTML = `
+            <td>${producto.nombre}</td>
+            <td>${producto.categoria}</td>
+            <td>${producto.precio}</td>
+            <td>${producto.cantidad}</td>
+            <td>${producto.codigo}</td>
+            <td>
+
+            </td>
+        `;
+
+        filaProducto.querySelector('td:last-child').appendChild(botonEliminar);
+        filaProducto.querySelector('td:last-child').appendChild(actualizarCantidad);
+        tabla.appendChild(filaProducto);
+    }
+
+    // Crear fila para el botón "Calcular Valor Total"
+    let filaCalcularTotal = document.createElement('tr');
+    filaCalcularTotal.innerHTML = `
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>
+        <button>Calcular Valor Total</button>
+        </td>
+    `;
+    tabla.appendChild(filaCalcularTotal);
+
+    // Crear fila vacía
+    let filaVacia = document.createElement('tr');
+    filaVacia.innerHTML = `
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    `;
+    tabla.appendChild(filaVacia);
+    
+}
+
+//AGREGAR PRODUCTOS
+function agregarProducto(event){
+
+//frenar que se actualice la pagina
+event.preventDefault();
+
+//Obtencion de datos del formulario
+let nombre = document.querySelector("#nombreProducto").value
+let categoria = document.querySelector("#categoriaProducto").value
+let precio = document.querySelector("#precioProducto").value
+let cantidad = document.querySelector("#cantidadProducto").value
+let codigo = document.querySelector("#codigoProducto").value
+
+let nuevoProducto = {
+    nombre: nombre,
+    categoria: categoria,
+    precio: precio,
+    cantidad: cantidad,
+    codigo: codigo
+};
+
+inventario.push(nuevoProducto);
+
+mostrarProductos();
+
+}
+
+//ELIMINAR PRODUCTOS
+function eliminarProducto(codigo) {
+    inventario = inventario.filter(producto => producto.codigo !== codigo);
+    mostrarProductos();
+}
+
+//ACTUALIZAR CANTIDAD DE PRODUCTO
+function actualizarCantidadProducto(codigo, cantNueva){
+    let producto = inventario.find(elemento => elemento.codigo==codigo)
+    if(producto){
+        producto.cantidad=cantNueva
+    }else{
+        console.log("NO se encontro el producto")
+    }
+}
+
+// Mostrar los productos al cargar la página
+document.addEventListener('DOMContentLoaded', mostrarProductos);
+
+//Ejecucion de la funcion Agregar Productos
+buttonAgregar.addEventListener('click', agregarProducto);
